f713599489bfa1c2c5e7b8bcf43fdcd2be4539ac
Benjamin Varga <ares9318@gmail.com>
2015-04-29 15:43:08 +0200

    vytvorenie entit

---
+++ src/cz/muni/fi/PB138/main/communication/ParserBarDrink.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.PB138.main.communication;
+
+import java.util.List;
+
+/**
+ * BarUTomaDataAnalyst
+ */
+public class ParserBarDrink implements Parser {
+
+    public List parse(String json) {
+        return null;
+    }
+
+    public boolean save(List list) {
+        return false;
+    }
+}
