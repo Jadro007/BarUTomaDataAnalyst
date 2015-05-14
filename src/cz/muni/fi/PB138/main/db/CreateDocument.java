9c3c9a7deacd98f0514cf875f1ec551f6f7855e4
Benjamin Varga <ares9318@gmail.com>
2015-05-14 13:17:41 +0200

    Added database

---
+++ src/cz/muni/fi/PB138/main/db/CreateDocument.java
@@ -0,0 +1,54 @@
+package cz.muni.fi.PB138.main.db;
+
+import java.io.File;
+
+import org.exist.xmldb.DatabaseInstanceManager;
+import org.exist.xmldb.EXistResource;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.*;
+import org.xmldb.api.*;
+public class CreateDocument {
+
+    private static String URI = "xmldb:exist:///db";
+
+
+    public static void main(String args[]) throws Exception {
+
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // initialize database driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        database.setProperty("configuration", System.getProperty("user.dir") + "\\database\\conf.xml");
+        DatabaseManager.registerDatabase(database);
+        Collection col = null;
+        XMLResource res = null;
+        try {
+            col = DatabaseManager.getCollection(URI + "//first", "admin", "");
+            XQueryService xqs = (XQueryService) col.getService("XQueryService", "1.0");
+
+            xqs.setProperty("indent", "yes");
+            // XQuery insert
+            CompiledExpression compiled = xqs.compile("update insert" + "<head>bla</head>"
+                    + "into //division[@did=\"production_zlin\"]");
+            ResourceSet result = xqs.execute(compiled);
+            System.out.println("ok.");
+        } finally {
+            //dont forget to cleanup
+            if(res != null) {
+                try { ((EXistResource)res).freeResources(); } catch(XMLDBException xe) {xe.printStackTrace();}
+            }
+
+            if(col != null) {
+                try { col.close(); } catch(XMLDBException xe) {xe.printStackTrace();}
+            }
+
+            DatabaseInstanceManager manager = (DatabaseInstanceManager)
+                    col.getService("DatabaseInstanceManager", "1.0");
+            manager.shutdown();
+        }
+    }
+
+}
+
---
+++ src/cz/muni/fi/PB138/main/db/TestDB.java
@@ -0,0 +1,32 @@
+package cz.muni.fi.PB138.main.db;
+
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.exist.xmldb.DatabaseInstanceManager;
+
+public class TestDB {
+    public static void main(String args[]) throws Exception {
+        // initialize driver
+        Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
+        Database database = (Database)cl.newInstance();
+        database.setProperty("create-database", "true");
+        database.setProperty("configuration", System.getProperty("user.dir") + "\\database\\conf.xml");
+        DatabaseManager.registerDatabase(database);
+
+        // try to read collection
+        Collection col =
+                DatabaseManager.getCollection("xmldb:exist:///db//first", "admin", "");
+        String resources[] = col.listResources();
+        System.out.println("Resources:");
+        for (int i = 0; i < resources.length; i++) {
+            System.out.println(resources[i]);
+            System.out.println(col.getResource(resources[i]).getContent());
+        }
+
+        // shut down the database
+        DatabaseInstanceManager manager = (DatabaseInstanceManager)
+                col.getService("DatabaseInstanceManager", "1.0");
+        manager.shutdown();
+    }
+}
