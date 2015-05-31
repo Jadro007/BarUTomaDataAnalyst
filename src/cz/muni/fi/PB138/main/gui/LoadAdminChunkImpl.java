11bd72b79648e066ba3caa99ca85d1d76588ffe2
Eva <smijakova.eva@gmail.com>
2015-05-31 12:07:36 +0200

    Base of GUI added

---
+++ src/cz/muni/fi/PB138/main/gui/LoadAdminChunkImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.PB138.main.gui;
+
+/**
+ * Created by Eva on 22.5.2015.
+ */
+public class LoadAdminChunkImpl {
+}
---
+++ src/cz/muni/fi/PB138/main/gui/LoadManager.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.PB138.main.gui;
+
+/**
+ * Created by Eva on 30.5.2015.
+ */
+public class LoadManager {
+}
